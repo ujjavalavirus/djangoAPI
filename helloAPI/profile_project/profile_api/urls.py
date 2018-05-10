1f5c8325d4c8e2c031796217e2596b2e15d36a63
Ujjaval <ujjaval@Ujjavals-MacBook-Pro.local>
2018-05-10 08:12:49 +0530

    code

---
+++ helloAPI/profile_project/profile_api/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.conf.urls import include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
+
+urlpatterns = [
+	url(r'^hello-view/',views.HelloApiView.as_view()),
+	url(r'',include(router.urls))
+]
